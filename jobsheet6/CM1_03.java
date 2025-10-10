000841f2b816355a0262fcf52bd6fd98cb1932cd
angelas606 <angelnyrt607@gmail.com>
2025-10-10 18:10:41 +0700

    Update CM1_03.java

--- jobsheet6/CM1_03.java
+++ jobsheet6/CM1_03.java
@@ -6,6 +6,7 @@ public class CM1_03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //Input data mahasiswa
         System.out.println("===== INPUT DATA MAHASISWA =====");
         System.out.print("Nama : ");
         String nama = sc.nextLine();
@@ -13,6 +14,7 @@ public class CM1_03 {
         int nim = sc.nextInt();
         System.out.println();
 
+        //Input nilai MK 1 Algoritma dan Pemrograman
         System.out.println("--- Mata Kuliah 1: Algoritma dan Pemrograman ---");
         System.out.print("Nilai UTS\t : ");
         int nilai_uts = sc.nextInt();
@@ -22,6 +24,7 @@ public class CM1_03 {
         int nilai_tugas = sc.nextInt();
         System.out.println();
 
+        //Input nilai MK 2 Struktur Data 
         System.out.println("--- Mata Kuliah 2: Struktur Data ---");
         System.out.print("Nilai UTS\t : ");
         int nilai_uts1 = sc.nextInt();
@@ -31,9 +34,11 @@ public class CM1_03 {
         int nilai_tugas1 = sc.nextInt();
         System.out.println();
 
+        //Menghitung nilai akhir 
         double nilai_akhir = (0.3 * nilai_uts) + (0.4 * nilai_uas) + (0.3 * nilai_tugas);
         double nilai_akhir1 = (0.3 * nilai_uts1) + (0.4 * nilai_uas1) + (0.3 * nilai_tugas1);
 
+        //Koversi nilai ke nilai huruf 
         String huruf1, huruf2;
         if (nilai_akhir >= 80 &&  nilai_akhir <= 100) {
             huruf1 = "A";
@@ -67,11 +72,14 @@ public class CM1_03 {
             huruf2 = "E";
         } 
 
+        //Status kelulusan per mata kuliah  
         String status1 = (nilai_akhir >= 60) ? "LULUS" : "TIDAK LULUS";
         String status2 = (nilai_akhir1 >= 60) ? "LULUS" : "TIDAK LULUS";
 
+        //Menghitung rata-rata nilai mata kuliah 
         double rata = (nilai_akhir + nilai_akhir1) / 2;
 
+        //Status kelulusan semester 
         String statusSemester;
         if (status1.equals("LULUS") && status1.equals("LULUS")) {
             if (rata >= 70) {
@@ -83,19 +91,20 @@ public class CM1_03 {
             statusSemester = "TIDAK LULUS (Salah satu mata kuliah TIDAK LULUS)";
         }
 
+        //Laporan nilai akhir 
         System.out.println("\n================ HASIL PENILAIAN AKADEMIK ================");
         System.out.println("Nama : " + nama);
         System.out.println("NIM  : " + nim);
-        System.out.println("-----------------------------------------------------------");
-        System.out.printf("%-20s %-5s %-5s %-7s %-12s %-12s %-10s\n",
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.printf("%-22s %-8s %-5s %-7s %-12s %-12s %-10s\n",
                 "Mata Kuliah", "UTS", "UAS", "Tugas", "Nilai Akhir", "Nilai Huruf", "Status");
-        System.out.println("-----------------------------------------------------------");
+        System.out.println("---------------------------------------------------------------------------------");
         System.out.println("Algoritma Pemrograman\t" + nilai_uts + "\t" + nilai_uas + "\t" + nilai_tugas + "\t" + 
-                           String.format("%.2f", nilai_akhir) + "\t" + huruf1 + "\t" + status1);
+                           String.format("%.2f", nilai_akhir) + "\t\t" + huruf1 + "\t" + status1);
         System.out.println("Struktur Data\t\t" + nilai_uts1 + "\t" + nilai_uas1 + "\t" + nilai_tugas1 + "\t" + 
-                           String.format("%.2f", nilai_akhir1) + "\t" + huruf2 + "\t" + status2);
-        System.out.println("-----------------------------------------------------------");
-        System.out.printf("\nRata-rata Nilai Akhir: %.2f\n", rata);
+                           String.format("%.2f", nilai_akhir1) + "\t\t" + huruf2 + "\t" + status2);
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.printf("\nRata-rata Nilai Akhir : %.2f\n", rata);
         System.out.println("Status Semester       : " + statusSemester);
 
     }
