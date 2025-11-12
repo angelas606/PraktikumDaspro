3e04543681192cf4c0846113e47c443a384815c3
angelas606 <angelnyrt607@gmail.com>
2025-11-12 14:17:38 +0700

    Pertanyaan 1

--- jobsheet10/SIAKAD03.java
+++ jobsheet10/SIAKAD03.java
@@ -6,7 +6,12 @@ public class SIAKAD03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[][] nilai = new int[4][3];
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int mahasiswa = sc.nextInt();
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int matkul = sc.nextInt();
+
+        int[][] nilai = new int[mahasiswa][matkul];
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i+1));
             double totalPerSiswa = 0;
@@ -17,20 +22,20 @@ public class SIAKAD03 {
                 totalPerSiswa += nilai[i][j];
             }
 
-            System.out.println("Nilai rata-rata: " + totalPerSiswa/3);
+            System.out.println("Nilai rata-rata: " + totalPerSiswa/nilai[0].length);
         }
 
         System.out.println("\n============================");
         System.out.println("Rata-rata Nilai setiap Mata Kuliah");
 
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < nilai[0].length; j++) {
             double totalPerMatkul = 0;
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < nilai.length; i++) {
                 totalPerMatkul += nilai[i][j];
             }
 
-            System.out.println("Mata Kuliah " + (j+1) + ": " + totalPerMatkul/4);
+            System.out.println("Mata Kuliah " + (j+1) + ": " + totalPerMatkul/nilai.length);
         }
     }
 }
