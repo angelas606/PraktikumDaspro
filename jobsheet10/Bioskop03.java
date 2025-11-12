e7028abc8f80ce04793b336fd1734dfe3c88193e
angelas606 <angelnyrt607@gmail.com>
2025-11-12 11:17:12 +0700

    Pertanyaan 9

--- jobsheet10/Bioskop03.java
+++ jobsheet10/Bioskop03.java
@@ -12,16 +12,8 @@ public class Bioskop03 {
         penonton[3][0] = "Gisel";
         penonton[3][1]= "Hana";
 
-        System.out.println(penonton.length);
-        for (String[] barisPenonton : penonton) {
-            System.out.println("Panjang baris: " + barisPenonton.length);
-        }
-
-        System.out.println();
-        System.out.println("Penonton pada baris ke-3: ");
-
-        for (String i : penonton[2]) {
-            System.out.println(i);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
         }
     }
 }
