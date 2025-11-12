cdbb7e0b3e778add04bfc7f1f2af177ff35e630a
angelas606 <angelnyrt607@gmail.com>
2025-11-12 16:31:19 +0700

    Pertanyaan 5

--- jobsheet10/BioskopWithScanner.java
+++ jobsheet10/BioskopWithScanner.java
@@ -44,7 +44,7 @@ public class BioskopWithScanner {
                     System.out.println("\n==== DAFTAR PENONTON ====");
                     for (int i = 0; i < penonton.length; i++) {
                         for (int j = 0; j < penonton[i].length; j++) {
-                            String isi = penonton[i][j];
+                            String isi = (penonton[i][j] == null) ? "***" : penonton[i][j];
                             System.out.print(isi + "\t");
                         }
                         System.out.println();
