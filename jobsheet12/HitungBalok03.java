b49902ea1352cbc54b43e867b0ba3979772b7d56
angelas606 <angelnyrt607@gmail.com>
2025-11-26 10:48:06 +0700

    Percobaan 5 dengan Fungsi

--- jobsheet12/HitungBalok03.java
+++ jobsheet12/HitungBalok03.java
@@ -3,6 +3,15 @@ package jobsheet12;
 import java.util.Scanner;
 
 public class HitungBalok03 {
+    static int hitungLuas(int panjang, int lebar) {
+        int Luas = panjang *lebar;
+        return Luas;
+    }
+
+    static int hitungVolume(int tinggi, int a, int b) {
+        int volume = hitungLuas(a, b) * tinggi;
+        return volume;
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int p, l, t, L, vol;
@@ -16,9 +25,9 @@ public class HitungBalok03 {
         System.out.print("Masukkan tinggi: ");
         t = input.nextInt();
 
-        L = p * l;
+        L = hitungLuas(p, l);
         System.out.println("Luas persegi panjang adalah " + L);
-        vol = p * l * t;
+        vol = hitungVolume(t, p, l);
         System.out.println("Volume balok adalah " + vol);
     }
 }
