11d8c2725b6b1f4e7c3456de2c40c9a06d627f72
angelas606 <angelnyrt607@gmail.com>
2025-11-26 09:26:12 +0700

    Percobaan  2

--- jobsheet12/Kafe03.java
+++ jobsheet12/Kafe03.java
@@ -1,7 +1,13 @@
 package jobsheet12;
 
 public class Kafe03 {
-    public static void main(String[] args) {
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("==== MENU RESTO KAFE ====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Capuccino - Rp 20,000");
@@ -11,5 +17,10 @@ public class Kafe03 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silakan pilih menu yang Anda inginkan");
+        
+    }
+
+    public static void main(String[] args) {
+        Menu("Andi", true);
     }
 }
